4f4325695ae696cb92ee84f2907619049c6cb544
mattpow35 <mpow2231@740s-pl-121.local>
2017-01-23 08:38:32 -0700

    started first C++ project

---
+++ FirstProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstProject
+//
+//  Created by Powley, Matthew on 1/23/17.
+//  Copyright © 2017 Powley, Matthew. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

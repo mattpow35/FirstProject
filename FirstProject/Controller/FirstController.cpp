960bc51ece102e488c727ea50a34967aa2fd034e
mattpow35 <mpow2231@740s-pl-121.local>
2017-01-25 09:49:04 -0700

    added some simple input and output

--- FirstProject/Controller/FirstController.cpp
+++ FirstProject/Controller/FirstController.cpp
@@ -15,4 +15,25 @@ using namespace std;
 void FirstController :: start()
 {
     cout << "goodbye world" << endl;
+    int myAge = 17;
+    cout << "I am " << myAge << " years old" << endl;
+    
+    this->specialOutput();
+}
+
+void FirstController :: specialOutput()
+{
+    cout << "how old are you" << endl;
+    int yourAge;
+    cin >> yourAge;
+    cout << "oh, so you are " << yourAge << " years old!" << endl;
+    string myName;
+    cin.ignore();
+    cout << "type in your name" << endl;
+    getline(cin, myName);
+    cout << "you typed: " << myName << endl;
+    cout << "Type in your name and age" << endl;
+    cin >> myName >> yourAge;
+    cout << "you typed: " << myName << " and " << yourAge << endl;
+
 }
